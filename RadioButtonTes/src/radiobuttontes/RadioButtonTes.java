ad45690484504b85649e56788c96426f32acdd02
Abdiel352 <120688631+Abdiel352@users.noreply.github.com>
2024-03-20 14:16:36 -0400

    Add files via upload

---
+++ RadioButtonTes/src/radiobuttontes/RadioButtonTes.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package radiobuttontes;
+
+/**
+ *
+ * @author Aluno
+ */
+import javax.swing.JFrame;
+public class RadioButtonTes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+        radioButtonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        radioButtonFrame.setSize(500,500);
+        radioButtonFrame.setVisible(true);
+    }
+    
+}
